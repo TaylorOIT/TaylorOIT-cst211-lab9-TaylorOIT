bf99f804cdbb65cdfb690d4a5e98cbfbca387010
Taylor Boyd <taylor.boyd@oit.edu>
2022-02-21 14:08:27 -0800

    Implemented the operator array index function and delete method.

--- BlankConsoleLab/BlankConsoleLab.cpp
+++ BlankConsoleLab/BlankConsoleLab.cpp
@@ -2,16 +2,28 @@
 //
 
 #include <iostream>
-#include "HashTable.h";
-
-using namespace std;
-
-using std::cout;
-using std::cin;
-using std::endl;
+#include "HashTable.h"
 
 int main()
 {
+	HashTable<string, Book> t(10);
 
+	Book temp = { "C++: Learn by Doing", "Todd Breedlove, Troy Scevers, et. al.", 635 };
+	Book temp1 = { "Rodeo for Dummies", "Calvin Caldwell", 1 };
+	Book temp2 = { "And That n There 2", "Ralph Carestia", 1 };
+	Book temp3 = { "And That n There", "Ralph Carestia", 1 };
+	t.Insert("0763757233", temp);
+	t.Insert("0763757233", temp1);
+	t.Insert("0763757234", temp3);
+	t.Insert("0763757234", temp2);
+
+	std::cout << t["7063757233"].m_title << std::endl;
+	std::cout << t["7063757233"].m_title << std::endl;
+	std::cout << t["7063757234"].m_title << std::endl;
+	std::cout << t["7063757234"].m_title << std::endl;
+
+	t.PrintHashTable();
+	t.Delete("7063757234");
+	t.PrintHashTable();
 }
 
--- BlankConsoleLab/HashTable.h
+++ BlankConsoleLab/HashTable.h
@@ -1,13 +1,12 @@
 #ifndef HASHTABLE_H
 #define HASHTABLE_H
+
 #include <string>
 #include <list>
-#include <vector>
+#include <iterator>
 using std::string;
 using std::list;
-using std::vector;
 using std::pair;
-
 
 struct Book {
 	string m_title;
@@ -15,22 +14,107 @@ struct Book {
 	int m_pages;
 };
 
-template <typename T, typename U>
+template <typename K, typename V>
 class HashTable {
 public:
-	HashTable(); // default constructor
-	HashTable(const HashTable<T,U>& copy); // copy constructor
-	HashTable(HashTable<T, U>&& copy); // move constructor
-	HashTable& operator =(const HashTable<T, U>& rhs); // copy assignment
-	HashTable& operator =(HashTable<T, U>&& rhs); // move assignment
+	HashTable(int value);
+	HashTable(const HashTable<K, V>& copy); // copy constructor
+	HashTable(HashTable<K, V>&& copy); // move constructor
+	HashTable& operator =(const HashTable<K, V>& rhs); // copy assignment
+	HashTable& operator =(HashTable<K, V>&& rhs); // move assignment
+
+	int setHash(K key); // returns the element where the list item will be inserted
+	void Insert(K key, V value); // inserts the object into the bucket
+	V operator [](K key); // returns the value depending on the key (operator array index)
+	void Delete(K key);
+	void Traverse(V value);
+	void PrintHashTable();
 private:
-	static const int buckets;
-	list<pair<T, U>> table[buckets];
+	list <pair<K, V> > *bucketlist;
+	int BUCKETSIZE;
+	int nxt_incr;
 };
 
+template<typename K, typename V>
+inline HashTable<K, V>::HashTable(int value) 
+{
+	nxt_incr = 0;
+	this->BUCKETSIZE = value;
+	bucketlist = new list <pair<K, V>>[BUCKETSIZE];
+}
 
+template<typename K, typename V>
+inline int HashTable<K, V>::setHash(K key)
+{
+	int hash = 0; 
+	int index;
 
+	index = key.length();
 
+	for (int i = 0; i < key.length(); i++)
+		hash = hash + (int)key[i];
 
+	index = hash % BUCKETSIZE;
+	
+	return index;
+}
+
+template<typename K, typename V>
+inline void HashTable<K, V>::Insert(K key, V value)
+{
+	int index = setHash(key);
+
+	pair<K, V> str_object_pair = make_pair(key, value);
+
+	bucketlist[index].push_back(str_object_pair);
+}
+
+template<typename K, typename V>
+inline V HashTable<K, V>::operator[](K key)
+{
+	int index = setHash(key);
+	for (int i = 0; i < BUCKETSIZE; i++) {
+		auto begin = bucketlist[i].begin();
+		auto nxt = std::next(begin, 0);
+
+		if (bucketlist[i].size() > 1 && index == i)
+		{
+			nxt = std::next(begin, nxt_incr);
+			++nxt_incr;
+			if (nxt_incr == bucketlist[i].size())
+				nxt_incr = 0;
+			return (*nxt).second;
+		}
+		else if (index == i) 
+		{
+			return (*nxt).second;
+		}
+	}
+}
+
+template<typename K, typename V>
+inline void HashTable<K, V>::Delete(K key)
+{
+	int index = setHash(key);
+	for (int i = 0; i < BUCKETSIZE; i++) 
+	{
+		if (index == i)
+			bucketlist[i].clear();
+	}
+}
+
+template<typename K, typename V>
+inline void HashTable<K, V>::PrintHashTable()
+{
+	std::cout << "\nHash Table:\n";
+	std::cout << "------------------------------------\n";
+	for (int i = 0; i < BUCKETSIZE; i++) {
+		std::cout << i;
+		for (auto list : bucketlist[i])
+			std::cout << " --> " << list.first << ", " << list.second.m_title;
+		std::cout << std::endl;
+	}
+	std::cout << "------------------------------------\n\n";
+}
 
 #endif
